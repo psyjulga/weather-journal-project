a8b2723dc39822d24d5629753126d3f07fb00bc1
👩Ladybug🐞 <psyjulga@yahoo.de>
2022-03-20 11:24:18 +0100

    first commit

---
+++ weather-journal-app/server.js
@@ -0,0 +1,48 @@
+// Setup empty JS object to act as endpoint for all routes
+const projectData = [];
+
+// const fetch = require("node-fetch"); => error, not supported
+import fetch from "node-fetch";
+
+// Express to run server and routes
+import express from "express";
+
+// Start up an instance of app
+const app = express();
+
+/* Dependencies */
+/* Middleware*/
+import bodyParser from "body-parser";
+import cors from "cors";
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static("website"));
+
+// Callback to debug
+const listening = () => console.log(`server running on port ${port}`);
+
+// Spin up the server
+const port = 8000;
+const server = app.listen(port, listening);
+
+// Callback function to complete GET '/all' ????
+const getData = (request, response) => response.send("test");
+// if we switch to /all "test" is displayed
+
+// Initialize all route with a callback function
+app.get("/all", getData);
+
+const postData = (request, response) => {
+  let data = request.body;
+  //projectData["myData"] = data.myData;
+};
+
+// Post Route
+app.post("/all", postData);
---
+++ weather-journal-app/website/app.js
@@ -0,0 +1,51 @@
+// to fix "document is not defined"
+window.addEventListener("DOMContentLoaded", loadApp, false);
+// window is not defined ????
+
+function loadApp() {
+  /* Global Variables */
+
+  // Personal API Key for OpenWeatherMap API
+
+  // Create a new date instance dynamically with JS
+  let d = new Date();
+  let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+  // access html elements
+
+  const dateEntry = document.getElementById("date");
+  const tempEntry = document.getElementById("temp");
+  const contentEntry = document.getElementById("content");
+  const submitButton = document.getElementById("generate");
+
+  const callApi = (zip) => {
+    const apiKey = "521e636942417dbc233358cdf12445eb";
+    const unit = "metric";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${zip}&appid=${apiKey}&units=${unit}`;
+    return fetch(apiUrl)
+      .then((response) => response.json())
+      .catch((ex) => console.log("error in callApi", ex));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // get user input
+    const zipInput = document.getElementById("zip").value;
+    console.log("zipInput in handleSubmit", zipInput);
+    const feelingsInput = document.getElementById("feelings").value;
+    const data = await callApi(zipInput);
+    console.log("data in hadleSubmit", data);
+  };
+
+  submitButton.addEventListener("submit", handleSubmit);
+
+  // Event listener to add function to existing HTML DOM element
+
+  /* Function called by event listener */
+
+  /* Function to GET Web API Data*/
+
+  /* Function to POST data */
+
+  /* Function to GET Project Data */
+}
